2d3c212e0202a612a418db0e1895c22cc2db82f8
Cyclone Addons <90061864+CycloneAddons@users.noreply.github.com>
2025-06-11 09:48:15 +0530

    Update jioSaavn.ts

--- api/jioSaavn.ts
+++ api/jioSaavn.ts
@@ -171,7 +171,7 @@ export class JioSaavnAPI {
 			albumName: null,
 			artistArtworkUrl: null,
 			previewUrl: null,
-			320kbps: track?.more_info?.320kbps,
+			"320kbps": track?.more_info?.["320kbps"],
 		};
 
 		if (track?.perma_url) {
